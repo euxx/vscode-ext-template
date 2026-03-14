a13508475caf43d5ed7184fca278de91916c3e62
eux <euxx@users.noreply.github.com>
2026-03-14 21:58:38 +0800

    feat: add extension entry point and unit tests

---
+++ src/extension.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Called when the extension is activated.
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(_context) {
+  // TODO: register commands, event listeners, etc.
+}
+
+function deactivate() {}
+
+module.exports = { activate, deactivate };
---
+++ tests/extension.test.js
@@ -0,0 +1,8 @@
+const { activate, deactivate } = require('../src/extension.js');
+
+describe('extension', () => {
+  it('exports activate and deactivate', () => {
+    expect(typeof activate).toBe('function');
+    expect(typeof deactivate).toBe('function');
+  });
+});
